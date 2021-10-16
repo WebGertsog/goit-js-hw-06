2006da7377774cd26197d120e7af1efb721d405b
WebGertsog <groxenvil@gmail.com>
2021-10-16 21:51:49 +0300

    task-09
    task-09 compleated

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', formFun);
+
+function formFun(event) {
+
+  event.preventDefault();
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert('Все поля должны быть заполнены!');
+  }
+
+  const dataBase = {
+    email: `${email.value}`,
+    password: `${password.value}`,
+  };
+  console.log(dataBase);
+  event.currentTarget.reset()
+};
+
+
 
